fd04a149c11c18ffacc9a3db4f5d0a8540f34d0b
Atriyo <atriyo.gogol@gmail.com>
2025-06-20 19:28:03 +0530

    added main file

--- Assignment 4/main.py
+++ Assignment 4/main.py
@@ -1,29 +1,27 @@
-from utils.place_info_search import TavilyPlaceSearchTool, GooglePlaceSearchTool
-from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage
+from agent.workflow import GraphBuilder
+from utils.save_to_document import save_document
 import os
-
-# llm = ChatOpenAI(
-#     model_name="o4-mini"
-# )
-
-# tavily_place_search_tool = TavilyPlaceSearchTool()
-google_place_search_tool = GooglePlaceSearchTool(os.environ["GPLACES_API_KEY"])
-
-# tools=[place_search_tool.search_attractions, 
-#        place_search_tool.search_restaurants, 
-#        place_search_tool.search_activity, 
-#        place_search_tool.search_transportation]
-# llm_with_tools=llm.bind_tools(tools)
-
-# SYSTEM_PROMPT="You are a helpful assistant tasked to get the attractions, restaurants, activities and transportations of a " \
-# "given location using the list of tools provided. Obtain the informations as per the user requirement and then segregate " \
-# "the output as per the attractions, restaurants, activities and transportations"
-
-# input_question = [SYSTEM_PROMPT]+[HumanMessage(content="Give me details about Kolkata")]
+import datetime
+from dotenv import load_dotenv
+load_dotenv()
 
 if __name__ == "__main__":
-    # response = llm_with_tools.invoke(input_question)
-    # print(response)
-    # gp = GooglePlaceSearchTool(os.environ["GPLACES_API_KEY"])
-    print(google_place_search_tool.search_restaurants("Kolkata"))
+    graph = GraphBuilder(model_provider="openai")
+    react_app = graph.build_graph()
+    png_graph = react_app.get_graph().draw_mermaid_png()
+    with open("my_graph.png", "wb") as f:
+        f.write(png_graph)
+
+    print(f"Graph saved as 'my_graph.png' in {os.getcwd()}")
+    query = """
+    Hi, I want to take a 12-day trip to Ladakh and Nubra Valley in August 2025. 
+    My hotel budget is around 1300 Rs per night person. I would mostly prefer homestays for night stay. Suggest me some
+    good homestay in Nubra Valley, Pangong and other remote places. 
+    I would like to know what the weather will be like, what places I can visit, and how much the whole trip might cost. 
+    Also, I prefer local food and public transportation. Can you plan it all for me?"
+    """
+    output = react_app.invoke({"messages":[query]})
+    print(output)
+    for m in output["messages"]:
+        m.pretty_print()
+    save_document(output["messages"][-1].content)
